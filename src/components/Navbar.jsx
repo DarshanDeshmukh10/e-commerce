adc4c8ed2b7b6d7784554bfa91c581aed14bb9e4
DarshanDeshmukh10 <deshmukhdarshan447@gmail.com>
2025-03-18 21:37:16 +0530

    setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='flex flex-row justify-between'>
+        <img src="" alt="" />
+        <div>
+            <p>Home</p>
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
